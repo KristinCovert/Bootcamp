7121069ec6af8f4aeefda16a2b4589a9e00268ed
KBratland <kristin.bratland@gmail.com>
2014-09-15 09:42:41 -0700

    Fibonacci calculation

--- fibonacciCalc.py
+++ fibonacciCalc.py
@@ -1,19 +1,28 @@
 __author__ = 'Kristin'
 
-def range_top():
-    range_top = input('Up to what number would you like to calculate the Fibonacci')
-    return range_top()
 
-def fibonacci():
+def range_top():
+    top_range = input('Up to what number would you like to calculate the Fibonacci: ')
+    return top_range
+
+
+def fibonacci(rt):
     i = 0
     j = 1
+
     fibonacci_list = []
-    for number in range(0, range_top()):
+
+    for number in range(0, rt):
         k = i + j
         fibonacci_list.append(k)
         i = j
         j = k
     return fibonacci_list
 
-fibonacci()
-print fibonacci()
+if __name__ == '__main__':
+    x = range_top()
+    result = fibonacci(x)
+    print result
+    l = len(result)
+    print l
+
