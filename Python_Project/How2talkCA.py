2ef28283b9e5f5561ed6112f5faee679e1d3f7a6
KBratland <kristin.bratland@gmail.com>
2014-09-22 14:09:42 -0700

    ver4

--- Python_Project/How2talkCA.py
+++ Python_Project/How2talkCA.py
@@ -55,54 +55,57 @@ def make_story(choice):
 class CASpeakTranslate():
     def __init__(self, story):
         self.story = story
-        self.locations = []
+    locations = []
+    text_split = []
+    joined_text = None
+    translated_story = None
 
 #take story and split it into a list but don't undo contractions using NLTK
-    def deconstruct_text(self, story):
-        text_split = RegexpTokenizer("[\w']+|[.,!?;:-]")
-        print(text_split.tokenize(story))
-        return text_split
+    def deconstruct_text(self):
+        self.text_split = RegexpTokenizer("[\w']+|[.,!?;:-]")
+        self.text_split = (self.text_split.tokenize(self.story))
+        return self.text_split
 
 #replace words with slang from dictionary (expand to reading big excel file)
-    def replace_word_slang(self, text_split):
+    def replace_word_slang(self):
         slang_dict = {'really': ['hella', 'totally', 'fully'], 'gross': ['grody', 'gag me']}
-        for word in text_split:
+        for word in self.text_split:
             for slang in slang_dict:
                 if word == slang:
                     print (word, random.choice(slang_dict[slang]))
                     slang_choice = random.choice(slang_dict[slang])
-                    text_split[text_split.index(word)] = slang_choice
-                    return text_split
+                    self.text_split[self.text_split.index(word)] = slang_choice
+                    return self.text_split
 
 #create random locations to replace with add-in words
-    def random_location(self, text_split, locations):
+    def random_location(self):
         count = 0
         while count < 5:
-            location = random.randint(1, len(text_split))
-            locations.append(location)
+            location = random.randint(1, len(self.text_split))
+            self.locations.append(location)
             count += 1
-        return locations
+        return self.locations
 
 #use locations made above to insert add-ins
-    def random_add(self, text_split):
+    def random_add(self):
         add_ins = [', so, ', ', like, ', ', OMG, ']
         for location in self.locations:
             add = random.choice(add_ins)
-            text_split.insert(location, add)
-        return text_split
+            self.text_split.insert(location, add)
+        return self.text_split
 
 #add in Dude after each !
-    def add_in_dude(self, text_split):
-        for element in text_split:
-            if element == '!':
-                text_split.insert(text_split.index(element), ', Dude!')
-                text_split.remove('!')
-        return text_split
+    # def add_in_dude(self, text_split):
+    #     for element in text_split:
+    #         if element == '!':
+    #             text_split.insert(text_split.index(element), ', Dude!')
+    #             text_split.remove('!')
+    #     return self.text_split
 
 #join the text
-    def join_text(self, text_split):
-        joined_text = ' '.join(text_split)
-        return joined_text
+    def join_text(self):
+        self.joined_text = ' '.join(self.text_split)
+        return self.joined_text
 
 #fix the punctuation
     def fix_punct(self, joined_text):
@@ -110,18 +113,18 @@ class CASpeakTranslate():
         fix_2 = re.sub(r"(\s+!)", "!", fix_1)
         fix_3 = re.sub(r"(\s+,)", ",", fix_2)
         fix_4 = re.sub(r"(\s+\?)", "?", fix_3)
-        done = re.sub(r"(\s+:)", ";", fix_4)
-        return done
+        translated_story = re.sub(r"(\s+:)", ";", fix_4)
+        return translated_story
 
 story = ca_speak_method()
 ca_speak = CASpeakTranslate(story)
 ca_speak.deconstruct_text(story)
-ca_speak.replace_word_slang(story)
-ca_speak.random_location(story, locations=[])
-ca_speak.random_add(story)
-ca_speak.add_in_dude(story)
-ca_speak.join_text(story)
-ca_speak.fix_punct(story)
+ca_speak.replace_word_slang(ca_speak.text_split)
+ca_speak.random_location(ca_speak.text_split, ca_speak.locations)
+ca_speak.random_add(ca_speak.text_split)
+#ca_speak.add_in_dude(story)
+ca_speak.join_text(ca_speak.text_split)
+ca_speak.fix_punct(ca_speak.text_split)
 print ca_speak
 
 #print ca_speak
