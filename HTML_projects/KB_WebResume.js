34913884f0faae6f10ed53d55ad33a91a49df87b
KBratland <kristin.bratland@gmail.com>
2014-10-15 11:30:47 -0700

    Resume on one pag ver2

--- HTML_projects/KB_WebResume.js
+++ HTML_projects/KB_WebResume.js
@@ -3,15 +3,63 @@
  */
 $(document).ready(function (){
 
-    //to-do: get all icons and loop through adding the JQuery event handler to them
-    //[icon,icon,icon].forEach(function(icon){$(icon).bind(function(){$(this).hover(function(){$(this).toggleClass(".icon-hover")})})})
+    $(".about-icon").hover(
+        function(){
+            $(this).attr({src: "/pictures/AboutIconUni.jpeg"})
+        },
+        function(){
+            $(this).attr({src: "/pictures/unicorn.jpg"})
+    });
 
-    var $icons = $("div.home-icons").children().children();
-    //console.log($icons);
+    function spinLarge () {
+        var angle = 0;
+        return setInterval(function(){
+            angle-=3;
+            $("#LG_cog").rotate(angle);
+            },50);
+    }
 
-    $(".icon").hover(function () {
-        $(this).css({
+    function spin () {
+        spinLarge()
+        var smallAngle = 0;
+        return setInterval(function () {
+            smallAngle += 3;
+            $("#SM_cog").rotate(smallAngle);
+        }, 50);
+    }
 
-        })
-    })
+//    I still can't get the wheel to stop turning but at least they start on hover
+    $(".cog-container").hover(
+        function() {
+            spin();
+        },
+        function() {
+                $("SM_cog", "LG_cog").rotate(0);
+      });
+
+    $("#codeguildBW").hover(
+        function() {
+            $(this).attr({src: "/pictures/PDX_CodeGuild.png"})
+        },
+            function() {
+                $(this).attr({src: "/pictures/PDX_CodeGuild_BW.png"})
+        });
+
+    $("#nauBW").hover(
+        function() {
+            $(this).attr({src: "/pictures/nau_color.png"})
+        },
+        function() {
+            $(this).attr({src: "/pictures/nau_BW.png"})
+
+    });
+
+    $("#ucscBW").hover(
+        function() {
+            $(this).attr({src: "/pictures/UCSC_color.jpg"})
+        },
+        function(){
+            $(this).attr({src: "/pictures/UCSC_bw.jpg"})
+    });
+
 });
